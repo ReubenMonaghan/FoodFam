fb815070057c4c6cf1de00b688993802347df2be
ReubenMonaghan <reuben.monaghan@nhs.net>
2023-10-13 20:40:59 +0100

    Comment, Rating and Group model added

--- FoodFam/FoodFam_App/models.py
+++ FoodFam/FoodFam_App/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 
@@ -15,3 +17,25 @@ class Recipe(models.Model):
     date_created = models.DateField()
     date_updated = models.DateField()
     owner = models.ForeignKey('Users', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    # check the on_delete settings for these
+    text = models.CharField(max_length=280)
+    date_created = models.DateField()
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+
+
+class Rating(models.Model):
+    # check the on_delete settings for these
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    Description = models.CharField(max_length=280)
+    date_created = models.DateField()
+    admins = models.ManyToManyField('Users', related_name='admin_groups')
