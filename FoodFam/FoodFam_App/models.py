c16d9c68ec78f667fff6157b84c212181477498f
ReubenMonaghan <reuben.monaghan@nhs.net>
2023-10-13 20:20:29 +0100

    Recipe model added

--- FoodFam/FoodFam_App/models.py
+++ FoodFam/FoodFam_App/models.py
@@ -8,3 +8,10 @@ class Users(models.Model):
     # profile_picture = models.ImageField()
 
 
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    ingredients = models.JSONField()
+    instructions = models.TextField(max_length=50)
+    date_created = models.DateField()
+    date_updated = models.DateField()
+    owner = models.ForeignKey('Users', on_delete=models.CASCADE)
