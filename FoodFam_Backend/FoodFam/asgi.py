5208a1eccc51a9dbec03c4d133bd11fe9e2db6bd
ReubenMonaghan <reuben.monaghan@nhs.net>
2023-11-15 15:33:45 +0000

    draft front page added

--- FoodFam_Backend/FoodFam/asgi.py
+++ FoodFam_Backend/FoodFam/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FoodFam project.
+ASGI config for FoodFam_Backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FoodFam.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FoodFam_Backend.settings')
 
 application = get_asgi_application()
--- FoodFam_Backend/FoodFam/wsgi.py
+++ FoodFam_Backend/FoodFam/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FoodFam project.
+WSGI config for FoodFam_Backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FoodFam.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FoodFam_Backend.settings')
 
 application = get_wsgi_application()
